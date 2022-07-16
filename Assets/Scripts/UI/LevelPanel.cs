89bf4fcecb94bbcc240b54cc4fbb7acb4d102fbe
zhshtabibito <zhshtabibito@foxmail.com>
2022-07-16 21:49:05 +0800

    link expand view text

--- Assets/Scripts/UI/LevelPanel.cs
+++ Assets/Scripts/UI/LevelPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelPanel : BasePanel
 {
@@ -8,6 +9,20 @@ public class LevelPanel : BasePanel
 
     public LevelPanel() : base(new PanelInfo(Path))
     {
+
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        PlayerController player = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        player.frontText = ActivePanel.Find("Num(1)").GetComponent<TMP_Text>();
+        player.leftText = ActivePanel.Find("Num(2)").GetComponent<TMP_Text>();
+        player.upText = ActivePanel.Find("Num(3)").GetComponent<TMP_Text>();
+        player.rightText = ActivePanel.Find("Num(4)").GetComponent<TMP_Text>();
+        player.backText = ActivePanel.Find("Num(5)").GetComponent<TMP_Text>();
+        player.bottomText = ActivePanel.Find("Num(6)").GetComponent<TMP_Text>();
 
     }
 }
